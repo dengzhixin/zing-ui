4bfc5390ad26257f9c4dcbe73841b234305c3561
deng <786339231@qq.com>
2020-08-02 17:21:49 +0800

    添加了icon

---
+++ src/Button.vue
@@ -0,0 +1,40 @@
+<template>
+    <button :class="{[`icon-${iconPosition}`]:true}">
+        <svg class="icon"  aria-hidden="true" v-if="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
+</template>
+<script lang="ts">
+    export default {
+        props:{
+            icon:String,
+            iconPosition:String
+        }
+    }
+</script>
+<style lang="scss" scoped>
+button {
+    display: inline-flex;
+    justify-content: center;
+    vertical-align: middle;
+    align-items: center;
+    > .icon{
+        order: 1;
+    }
+    > .content{
+        order: 2;
+    }
+    &.icon-right{
+        .content{
+            order: 1;
+        }
+        .icon{
+            order: 2;
+        }
+    }
+}
+</style>
---
+++ src/app.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Button from "./Button";
+Vue.component('z-button',Button)
+new Vue({
+    el:'#app'
+})
